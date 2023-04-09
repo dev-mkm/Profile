cd29ac7858d5ff31e53914465ce338c370035407
MKM <m.khoshdel81@gmail.com>
2023-04-09 12:24:56 +0330

    Profile Update

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -5,5 +5,5 @@ import CardComponent from '../components/CardComponent.vue';
 <template>
   <CardComponent header="Mohammad Khoshdel" text="Junior/Mid-Level Backend Developer | 20 Years old" mainheader="true"/>
   <CardComponent header="Education" text="Computer Science Student at University of Qom"/>
-  <CardComponent header="Careers that I'm intrested in" text="Part-Time | Back-end Developer | Laravel/NodeJS Frameworks"/>
+  <CardComponent header="Careers that I'm on the lookout for" text="Part-Time Jobs | Back-end Developer | Laravel/NodeJS Frameworks"/>
 </template>
