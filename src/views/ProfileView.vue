08732a2476e05f1cf7d374a39c37c95fb2a9fbf8
MKM <m.khoshdel81@gmail.com>
2023-05-16 14:16:35 +0330

    Update Info

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,7 +3,7 @@ import CardComponent from '../components/CardComponent.vue';
 </script>
 
 <template>
-  <CardComponent header="Mohammad Khoshdel" text="Junior/Mid-Level Backend Developer | 20 Years old" mainheader="true"/>
+  <CardComponent header="Mohammad Khoshdel" text="Junior Backend Developer | 20 Years old" mainheader="true"/>
   <CardComponent header="Education" text="Computer Science Student at University of Qom"/>
   <CardComponent header="Careers that I'm on the lookout for" text="Part-Time Jobs | Back-end Developer | Laravel/NodeJS Frameworks"/>
 </template>
