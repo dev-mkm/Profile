49a1e77608470ac0eb5ab9046d4840bd50b61c9c
MKM <m.khoshdel81@gmail.com>
2023-07-08 17:27:47 +0330

    Update

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -5,19 +5,19 @@ import SkillComponent from '../components/SkillComponent.vue';
 <template>
   <div class="div">
     <h2 class="head">Skills</h2>
-    <SkillComponent skill="PHP" experience="3.7" base="5" red="15" green="171" blue="255"/>
-    <SkillComponent skill="Python" experience="2" base="5" red="15" green="171" blue="255"/>
-    <SkillComponent skill="C#" experience="1.5" base="5" red="15" green="171" blue="255"/>
-    <SkillComponent skill="Javascript" experience="0.7" base="5" red="15" green="171" blue="255"/>
-    <SkillComponent skill="Kotlin" experience="0.3" base="5" red="15" green="171" blue="255"/>
+    <SkillComponent skill="PHP" experience="2.7" base="5" red="15" green="171" blue="255"/>
+    <SkillComponent skill="Python" experience="1.7" base="5" red="15" green="171" blue="255"/>
+    <SkillComponent skill="C#" experience="0.7" base="5" red="15" green="171" blue="255"/>
+    <SkillComponent skill="Javascript" experience="0.4" base="5" red="15" green="171" blue="255"/>
+    <SkillComponent skill="Kotlin" experience="0.2" base="5" red="15" green="171" blue="255"/>
   </div>
   <div class="div" style="float: right;">
     <h2 class="head">Frameworks</h2>
-    <SkillComponent skill="Rest API" experience="2.5" base="4" red="15" green="255" blue="86"/>
-    <SkillComponent skill="Laravel" experience="1.2" base="4" red="15" green="255" blue="86"/>
-    <SkillComponent skill="Django" experience="0.7" base="4" red="15" green="255" blue="86"/>
-    <SkillComponent skill="Vue" experience="0.4" base="4" red="15" green="255" blue="86"/>
-    <SkillComponent skill="NodeJS" experience="0.2" base="4" red="15" green="255" blue="86"/>
+    <SkillComponent skill="Rest API" experience="2" base="4" red="15" green="255" blue="86"/>
+    <SkillComponent skill="Laravel" experience="1.5" base="4" red="15" green="255" blue="86"/>
+    <SkillComponent skill="Vue" experience="0.5" base="4" red="15" green="255" blue="86"/>
+    <SkillComponent skill="NodeJS" experience="0.3" base="4" red="15" green="255" blue="86"/>
+    <SkillComponent skill="Django" experience="0.3" base="4" red="15" green="255" blue="86"/>
   </div>
 </template>
 
